569190538809b26649430766b7877fd03a9d8322
TamaraBozhanova <tamarab@123software.ru>
2019-09-28 00:03:07 +0300

    update forms for all projects

--- CollectionViewDemo/CollectionViewDemo/App.xaml.cs
+++ CollectionViewDemo/CollectionViewDemo/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CollectionViewDemo
         {
             InitializeComponent();
 
-            MainPage = new Views.MainPage();
+            MainPage = new NavigationPage(new Views.MainPage());
         }
 
         protected override void OnStart()
