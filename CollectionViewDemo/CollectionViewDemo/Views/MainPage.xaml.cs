3ce9d823eaba522ef5ba81f50cda5b1e2403d7a0
TamaraBozhanova <tamarab@123software.ru>
2019-09-27 23:01:56 +0300

    added TableView

--- CollectionViewDemo/CollectionViewDemo/Views/MainPage.xaml.cs
+++ CollectionViewDemo/CollectionViewDemo/Views/MainPage.xaml.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CollectionViewDemo.Views
 {
     public partial class MainPage : ContentPage
     {
+        public ICommand NavigateCommand { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            NavigateCommand = new Command<Type>(
+                async (Type pageType) =>
+                {
+                    Page page = (Page)Activator.CreateInstance(pageType);
+                    await Navigation.PushAsync(page);
+                });
+
+            BindingContext = this;
         }
     }
 }
