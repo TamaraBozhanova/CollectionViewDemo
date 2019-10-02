69479dab466c2394221e6f6b9d273f277fa6bcb0
TamaraBozhanova <tamarab@123software.ru>
2019-10-02 23:07:03 +0300

    added horizontal grid page

---
+++ CollectionViewDemo/CollectionViewDemo/Views/Layout/HorizontalGridPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CollectionViewDemo.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViewDemo.Views.Layout
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HorizontalGridPage : ContentPage
+	{
+		public HorizontalGridPage ()
+		{
+			InitializeComponent ();
+            BindingContext = new MonkeysViewModel();
+        }
+	}
+}
