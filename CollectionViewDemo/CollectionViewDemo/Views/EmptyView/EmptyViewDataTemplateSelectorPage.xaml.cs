5caecf3ae51ff8496c9c464a5f298e35f2a45b72
TamaraBozhanova <tamarab@123software.ru>
2019-10-18 19:00:22 +0300

    no message

---
+++ CollectionViewDemo/CollectionViewDemo/Views/EmptyView/EmptyViewDataTemplateSelectorPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CollectionViewDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CollectionViewDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmptyViewDataTemplateSelectorPage : ContentPage
+    {
+        public EmptyViewDataTemplateSelectorPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+    }
+}
